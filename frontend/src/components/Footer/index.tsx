7549cde4bff0b572bd9b46a1f546465d62e022c4
Andre Santos <andresanto.dev@gmail.com>
2021-05-05 22:06:38 -0300

    user author changed

--- frontend/src/components/Footer/index.tsx
+++ frontend/src/components/Footer/index.tsx
@@ -2,7 +2,7 @@ const Footer = () => {
     return (
         <footer className="footer mt-auto py-3 bg-dark">
             <div className="container">
-                <p className="text-light">App desenvolvido por <a href="https://github.com/acenelio" target="_blank" rel="noreferrer">André Santos</a></p>
+                <p className="text-light">App desenvolvido por <a href="https://github.com/CodeSantos01" target="_blank" rel="noreferrer">André Santos</a></p>
                 <p className="text-light"><small><strong>Semana Spring React</strong><br />
             Evento promovido pela escola DevSuperior: <a href="https://instagram.com/devsuperior.ig" target="_blank" rel="noreferrer">@devsuperior.ig</a></small></p>
             </div>
