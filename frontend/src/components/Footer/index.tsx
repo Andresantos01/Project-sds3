7e46250605b38a75371245ba3539bed99f0be3a8
Andre Santos <andresanto.dev@gmail.com>
2021-05-09 16:09:50 -0300

    instagram changed

--- frontend/src/components/Footer/index.tsx
+++ frontend/src/components/Footer/index.tsx
@@ -4,7 +4,7 @@ const Footer = () => {
             <div className="container">
                 <p className="text-light">App desenvolvido por <a href="https://github.com/CodeSantos01" target="_blank" rel="noreferrer">André Santos</a></p>
                 <p className="text-light"><small><strong>Semana Spring React</strong><br />
-            Evento promovido pela escola DevSuperior: <a href="https://instagram.com/devsuperior.ig" target="_blank" rel="noreferrer">@devsuperior.ig</a></small></p>
+            Evento promovido pela escola DevSuperior: <a href="https://www.instagram.com/andresanto.dev/" target="_blank" rel="noreferrer">@andresanto.dev</a></small></p>
             </div>
         </footer>
     );
